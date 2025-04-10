819a0b27ca00233de031a457fc719876fadf2503
ashaaut <ashaautade1998@gmail.com>
2025-04-10 16:50:09 +0530

    Implement sum calculation for valid comma-separated numbers

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,24 @@
+import { useState } from "react";
+
 function App() {
+  const [inputString, setInputString] = useState(""); 
+  const [sum, setSum] = useState(0);
+
+  const calculateSum = (input) => {
+    if (!input.trim()) {
+      setSum(0);
+      return;
+    }
+    const numbersArray = input.split(",").map((number) => parseInt(number));
+    let totalSum = numbersArray.reduce((acc, curr) => acc + curr, 0);
+    setSum(totalSum);
+  };
+
   return (
     <div className="App">
-     String Calculator
+      <input onChange={(e) => setInputString(e.target.value)} />
+      <button onClick={() => calculateSum(inputString)}>Calculate</button>
+      <div>Result: {sum}</div>
     </div>
   );
 }
--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,9 +1,52 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-import '@testing-library/jest-dom'
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "../App";
+import "@testing-library/jest-dom";
 
-test('render string calculator text', () => {
+test("should render input and calculate button", () => {
   render(<App />);
-  const element = screen.getByText("String Calculator");
-  expect(element).toBeInTheDocument();
+
+  const inputElement = screen.getByRole("textbox");
+  const calculateButton = screen.getByRole("button");
+
+  expect(inputElement).toBeInTheDocument();
+  expect(calculateButton).toBeInTheDocument();
 });
+
+test("should return 0 if empty value is provided", () => {
+  render(<App />);
+
+  const inputElement = screen.getByRole("textbox");
+  const calculateButton = screen.getByRole("button");
+
+  fireEvent.change(inputElement, {target:{value:""}});
+  fireEvent.click(calculateButton);
+
+  const result = screen.getByText("Result: 0");
+  expect(result).toBeInTheDocument();
+});
+
+test("should return the number when a single number is provided",()=>{
+  render(<App/>)
+
+  const inputElement = screen.getByRole("textbox");
+  const calculateButton = screen.getByRole("button");
+
+  fireEvent.change(inputElement, {target:{value:"1"}});
+  fireEvent.click(calculateButton);
+
+  const result=screen.getByText("Result: 1");
+  expect(result).toBeInTheDocument();
+});
+
+test("should return the addition of numbers when multiple valid numbers are provided",()=>{
+  render(<App/>)
+
+  const inputElement = screen.getByRole("textbox");
+  const calculateButton = screen.getByRole("button");
+
+  fireEvent.change(inputElement, {target:{value:"1,5"}});
+  fireEvent.click(calculateButton);
+
+  const result=screen.getByText("Result: 6");
+  expect(result).toBeInTheDocument();
+})
