5c4434eb276865a6f52ed0f36b95eedfc01369a6
ashaaut <ashaautade1998@gmail.com>
2025-04-12 18:55:22 +0530

    Add tailwind for styling

--- src/components/App.js
+++ src/components/App.js
@@ -15,11 +15,29 @@ function App() {
     }
   };
 
-  return (
-    <div className="App">
-      <textarea onChange={(e) => setInputString(e.target.value)} />
-      <button onClick={handleCalculate}>Calculate</button>
-      {error ? <div>{error}</div> : <div>Result: {sum}</div>}
+   return (
+    <div className="flex flex-col items-center my-16">
+      <h1 className="font-bold text-3xl">String Calculater</h1>
+      <div className="m-4 p-4 flex flex-col items-center border border-gray-100 w-1/3 shadow-lg  ">
+        <div className="flex">
+          <textarea
+            className="border border-gray-400 p-2  rounded-lg"
+            type="text"
+            onChange={(e) => setInputString(e.target.value)}
+          />
+          <button
+            className="bg-gray-300  rounded-lg m-2 px-2 text-lg"
+            onClick={handleCalculate}
+          >
+            Calculate
+          </button>
+        </div>
+        {error ? (
+          <div className="text-red-500 mt-2">{error}</div>
+        ) : (
+          <div className="text-lg font-bold my-4">Result: {sum}</div>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,104 +2,106 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./../App";
 import "@testing-library/jest-dom";
 
-test("should render input and calculate button", () => {
-  render(<App />);
+describe("String calculator", () => {
+  test("should render input and calculate button", () => {
+    render(<App />);
 
-  const inputElement = screen.getByRole("textbox");
-  const calculateButton = screen.getByRole("button");
+    const inputElement = screen.getByRole("textbox");
+    const calculateButton = screen.getByRole("button");
 
-  expect(inputElement).toBeInTheDocument();
-  expect(calculateButton).toBeInTheDocument();
-});
+    expect(inputElement).toBeInTheDocument();
+    expect(calculateButton).toBeInTheDocument();
+  });
 
-test("should return 0 if empty value is provided", () => {
-  render(<App />);
+  test("should return 0 if empty value is provided", () => {
+    render(<App />);
 
-  const inputElement = screen.getByRole("textbox");
-  const calculateButton = screen.getByRole("button");
+    const inputElement = screen.getByRole("textbox");
+    const calculateButton = screen.getByRole("button");
 
-  fireEvent.change(inputElement, { target: { value: " " } });
-  fireEvent.click(calculateButton);
+    fireEvent.change(inputElement, { target: { value: " " } });
+    fireEvent.click(calculateButton);
 
-  const result = screen.getByText("Result: 0");
-  expect(result).toBeInTheDocument();
-});
+    const result = screen.getByText("Result: 0");
+    expect(result).toBeInTheDocument();
+  });
 
-test("should return the number when a single number is provided", () => {
-  render(<App />);
+  test("should return the number when a single number is provided", () => {
+    render(<App />);
 
-  const inputElement = screen.getByRole("textbox");
-  const calculateButton = screen.getByRole("button");
+    const inputElement = screen.getByRole("textbox");
+    const calculateButton = screen.getByRole("button");
 
-  fireEvent.change(inputElement, { target: { value: "1" } });
-  fireEvent.click(calculateButton);
+    fireEvent.change(inputElement, { target: { value: "1" } });
+    fireEvent.click(calculateButton);
 
-  const result = screen.getByText("Result: 1");
-  expect(result).toBeInTheDocument();
-});
+    const result = screen.getByText("Result: 1");
+    expect(result).toBeInTheDocument();
+  });
 
-test("should return the addition of numbers when multiple comma separeted valid numbers are provided", () => {
-  render(<App />);
+  test("should return the addition of numbers when multiple comma separeted valid numbers are provided", () => {
+    render(<App />);
 
-  const inputElement = screen.getByRole("textbox");
-  const calculateButton = screen.getByRole("button");
+    const inputElement = screen.getByRole("textbox");
+    const calculateButton = screen.getByRole("button");
 
-  fireEvent.change(inputElement, { target: { value: "1,5,6" } });
-  fireEvent.click(calculateButton);
+    fireEvent.change(inputElement, { target: { value: "1,5,6" } });
+    fireEvent.click(calculateButton);
 
-  const result = screen.getByText("Result: 12");
-  expect(result).toBeInTheDocument();
-});
+    const result = screen.getByText("Result: 12");
+    expect(result).toBeInTheDocument();
+  });
 
-test("should return the addition of numbers when multiple valid numbers  provided in new lines", () => {
-  render(<App />);
+  test("should return the addition of numbers when multiple valid numbers  provided in new lines", () => {
+    render(<App />);
 
-  const inputElement = screen.getByRole("textbox");
-  const calculateButton = screen.getByRole("button");
+    const inputElement = screen.getByRole("textbox");
+    const calculateButton = screen.getByRole("button");
 
-  fireEvent.change(inputElement, { target: { value: "1\n2\n5\n3" } });
-  fireEvent.click(calculateButton);
+    fireEvent.change(inputElement, { target: { value: "1\n2\n5\n3" } });
+    fireEvent.click(calculateButton);
 
-  const result = screen.getByText("Result: 11");
-  expect(result).toBeInTheDocument();
-});
+    const result = screen.getByText("Result: 11");
+    expect(result).toBeInTheDocument();
+  });
 
-test("should return the addition of numbers when multiple valid numbers are provided in new lines or comma separated", () => {
-  render(<App />);
+  test("should return the addition of numbers when multiple valid numbers are provided in new lines or comma separated", () => {
+    render(<App />);
 
-  const inputElement = screen.getByRole("textbox");
-  const calculateButton = screen.getByRole("button");
+    const inputElement = screen.getByRole("textbox");
+    const calculateButton = screen.getByRole("button");
 
-  fireEvent.change(inputElement, { target: { value: "1\n2\n5,3" } });
-  fireEvent.click(calculateButton);
+    fireEvent.change(inputElement, { target: { value: "1\n2\n5,3" } });
+    fireEvent.click(calculateButton);
 
-  const result = screen.getByText("Result: 11");
-  expect(result).toBeInTheDocument();
-});
+    const result = screen.getByText("Result: 11");
+    expect(result).toBeInTheDocument();
+  });
 
-test("should return sum of numbers when beginning of the string will contain a separate line for delimiter", () => {
-  render(<App />);
-  global.alert = jest.fn();
+  test("should return sum of numbers when beginning of the string will contain a separate line for delimiter", () => {
+    render(<App />);
+    global.alert = jest.fn();
 
-  const inputElement = screen.getByRole("textbox");
-  const calculateButton = screen.getByRole("button");
+    const inputElement = screen.getByRole("textbox");
+    const calculateButton = screen.getByRole("button");
 
-  fireEvent.change(inputElement, { target: { value: "//;\n1;3;5" } });
-  fireEvent.click(calculateButton);
+    fireEvent.change(inputElement, { target: { value: "//;\n1;3;5" } });
+    fireEvent.click(calculateButton);
 
-  const result = screen.getByText("Result: 9");
-  expect(result).toBeInTheDocument();
-});
+    const result = screen.getByText("Result: 9");
+    expect(result).toBeInTheDocument();
+  });
 
-test("should throw an error when negative numbers are provided", () => {
-  render(<App />);
+  test("should throw an error when negative numbers are provided", () => {
+    render(<App />);
 
-  const inputElement = screen.getByRole("textbox");
-  const calculateButton = screen.getByRole("button");
+    const inputElement = screen.getByRole("textbox");
+    const calculateButton = screen.getByRole("button");
 
-  fireEvent.change(inputElement, { target: { value: "-2,4,-6" } });
-  fireEvent.click(calculateButton);
+    fireEvent.change(inputElement, { target: { value: "-2,4,-6" } });
+    fireEvent.click(calculateButton);
 
-  const result = screen.getByText("Negative numbers are not allowed: -2,-6");
-  expect(result).toBeInTheDocument();
+    const result = screen.getByText("Negative numbers are not allowed: -2,-6");
+    expect(result).toBeInTheDocument();
+  });
 });
